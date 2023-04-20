d7c03bfa4067bc9383414a59c802c0565207e101
marcuswalrod <walrodme01@sunyjcc.edu>
2023-04-20 16:13:31 -0400

    Update colorSound.py

--- colorSound.py
+++ colorSound.py
@@ -3,3 +3,17 @@
 # Date: 04-20-2023
 # Version 0.1a
 #-------------------------------------------#
+
+
+#Import Libraries
+#See if they want to use own image or sample
+#See if we have an image
+#Save image into a temp file
+#Ask computer to view the image
+#Grab hue from HSV
+#Separate the hues and convert to frequency
+#Generic song save
+#Make an affected version
+#Save the final song and make a midi
+#Get user input
+

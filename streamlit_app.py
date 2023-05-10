4d8c1e0dab31a6f2e02eabae0fb58df1acb3e80f
marcuswalrod <walrodme01@sunyjcc.edu>
2023-05-09 22:06:28 -0400

    Comment out pass filter

--- streamlit_app.py
+++ streamlit_app.py
@@ -341,11 +341,11 @@ with ps1:
     semitones   = st.slider('semitones', min_value=0.0, max_value=12.0, step=1.0, value=0.0) 
 
 
-col = st.columns(2)
+col = st.columns(1)
 
-with col:
+'''with col:
     st.markdown("### :blue[Lowpass Filter]")
-    pass_filter = st.slider('cutoff frequency', min_value=0.0, max_value=1.0, step=0.1, value=0.0)
+    pass_filter = st.slider('cutoff frequency', min_value=0.0, max_value=1.0, step=0.1, value=0.0)'''
 
 # Making the required prediction
 if img2load is not None:

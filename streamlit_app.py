995dfb0bcc636eeab75d49c275e19798fcb3a6c4
marcuswalrod <walrodme01@sunyjcc.edu>
2023-05-11 20:46:52 -0400

    Add gsheets

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,6 +29,15 @@ from scipy.io import wavfile
 import librosa
 import glob
 
+@st.cache_data(ttl=600)
+def load_data(sheets_url):
+    csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
+    return pd.read_csv(csv_url)
+
+df = load_data(st.secrets["public_gsheets_url"])
+
+for row in df.itertuples():
+    st.write(f"{row.name} has a :{row.pet}:")
 
 #This function generates frequencies in Hertz from notes
 def get_piano_notes():   

d5611265a67e7b80afa70c93ee2a88a917246987
marcuswalrod <walrodme01@sunyjcc.edu>
2023-05-11 21:02:43 -0400

    Remove last line

--- streamlit_app.py
+++ streamlit_app.py
@@ -418,8 +418,8 @@ hide_st_style = """
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
 @st.cache_data(ttl=600)
+
 def load_data(sheets_url):
     csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
     return pd.read_csv(csv_url)
 
-df = load_data(st.secrets["public_gsheets_url"])

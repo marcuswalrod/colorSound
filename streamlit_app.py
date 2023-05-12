218a7fdd962e374dca8cad092e9e16c7250ef046
marcuswalrod <walrodme01@sunyjcc.edu>
2023-05-11 21:07:55 -0400

    Add load_data

--- streamlit_app.py
+++ streamlit_app.py
@@ -423,3 +423,4 @@ def load_data(sheets_url):
     csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
     return pd.read_csv(csv_url)
 
+df = load_data(st.secrets["public_gsheets_url"])
